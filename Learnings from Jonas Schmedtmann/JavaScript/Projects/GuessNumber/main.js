93af867dc5e7b453dcf89f05592c8601aab40001
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-03-22 18:44:27 +0545

    Refactor code with Variables in CSS

--- Learnings from Jonas Schmedtmann/JavaScript/Projects/GuessNumber/main.js
+++ Learnings from Jonas Schmedtmann/JavaScript/Projects/GuessNumber/main.js
@@ -2,57 +2,49 @@
 let actualNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+let mainBackgroundStyle = document.querySelector(".main");
+let message = document.querySelector(".main-content__message");
+let actualNumberMsg = document.querySelector(".main-content__actual-number");
+let inputBackground = document.querySelector(".Guess");
+let scoreMsg = document.querySelector(".main-content__score");
+let highScoreMsg = document.querySelector(".main-content__highscore");
 
-function checkNumber() {
-  let guessNumber = Number(document.querySelector(".Guess").value);
-  if (actualNumber === guessNumber) {
-    if (highScore < score) highScore = score;
-    document.querySelector(".main").style.backgroundColor = "green";
-    document.querySelector(".main-content__message").innerHTML =
-      "🎉 Correct Guess!!";
-    document.querySelector(".main-content__actual-number").innerText =
-      actualNumber;
-    document.querySelector(".Guess").style.backgroundColor = "green";
-    document.querySelector(".main-content__score").innerHTML = score;
-    document.querySelector(".main-content__highscore").innerHTML = highScore;
-  } else if (guessNumber > 20 || guessNumber <= 0) {
-    document.querySelector(".main-content__message").innerHTML =
-      "⛔Number Out of Range";
-  } else if (guessNumber > actualNumber) {
-    if (score > 1) {
-      document.querySelector(".main-content__message").innerHTML =
-        "📈Too High..";
-      score--;
-      document.querySelector(".main-content__score").innerHTML = score;
-    } else {
-      document.querySelector(".main-content__score").innerHTML = "0";
-      document.querySelector(".main-content__message").innerHTML =
-        "💥GAME OVER!!";
+document
+  .querySelector(".main-content__check")
+  .addEventListener("click", function () {
+    let guessNumber = Number(document.querySelector(".Guess").value);
+    if (actualNumber === guessNumber) {
+      if (highScore < score) highScore = score;
+      mainBackgroundStyle.style.backgroundColor = "green";
+      message.textContent = "🎉 Correct Guess!!";
+      actualNumberMsg.textContent = actualNumber;
+      inputBackground.style.backgroundColor = "green";
+      scoreMsg.textContent = score;
+      highScoreMsg.textContent = highScore;
+    } else if (guessNumber > 20 || guessNumber <= 0 || !guessNumber)
+      message.textContent = "⛔Number Out of Range";
+    else if (guessNumber != actualNumber) {
+      if (score > 1) {
+        message.textContent =
+          guessNumber > actualNumber ? "📈Too High.." : "📉Too Low..";
+        score--;
+        scoreMsg.textContent = score;
+      } else {
+        scoreMsg.textContent = "0";
+        message.textContent = "💥GAME OVER!!";
+      }
     }
-  } else if (guessNumber < actualNumber) {
-    if (score > 1) {
-      document.querySelector(".main-content__message").innerHTML =
-        "📉Too Low..";
-      score--;
-      document.querySelector(".main-content__score").innerHTML = score;
-    } else {
-      document.querySelector(".main-content__score").innerHTML = "0";
-      document.querySelector(".main-content__message").innerHTML =
-        "💥GAME OVER!!";
-    }
-  }
-}
+  });
 
 document
   .querySelector(".header__play-again")
   .addEventListener("click", function () {
     actualNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector(".main-content__actual-number").innerText = "?";
-    document.querySelector(".main-content__message").innerHTML =
-      "Start Guessing...";
-    document.querySelector(".main").style.backgroundColor = "black";
-    document.querySelector(".Guess").style.backgroundColor = "black";
+    actualNumberMsg.textContent = "?";
+    message.textContent = "Start Guessing...";
+    mainBackgroundStyle.style.backgroundColor = "black";
+    inputBackground.style.backgroundColor = "black";
     document.querySelector(".Guess").value = "";
-    score = 20;
-    document.querySelector(".main-content__score").innerHTML = score;
+    score = "20";
+    scoreMsg.textContent = score;
   });
