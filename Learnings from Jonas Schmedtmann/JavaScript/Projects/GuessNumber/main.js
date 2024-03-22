a549d2878d030348c7b3d47692c9e59cedb6edb7
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-03-22 15:53:05 +0545

    Reach Full Functionality

--- Learnings from Jonas Schmedtmann/JavaScript/Projects/GuessNumber/main.js
+++ Learnings from Jonas Schmedtmann/JavaScript/Projects/GuessNumber/main.js
@@ -1,26 +1,32 @@
 "use strict";
 let actualNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 0;
+let score = 20;
 let highScore = 0;
 
 function checkNumber() {
   let guessNumber = Number(document.querySelector(".Guess").value);
   if (actualNumber === guessNumber) {
+    if (highScore < score) highScore = score;
     document.querySelector(".main").style.backgroundColor = "green";
     document.querySelector(".main-content__message").innerHTML =
       "🎉 Correct Guess!!";
     document.querySelector(".main-content__actual-number").innerText =
       actualNumber;
     document.querySelector(".Guess").style.backgroundColor = "green";
-    score++;
     document.querySelector(".main-content__score").innerHTML = score;
+    document.querySelector(".main-content__highscore").innerHTML = highScore;
   } else if (guessNumber > 20 || guessNumber <= 0) {
     document.querySelector(".main-content__message").innerHTML =
       "⛔Number Out of Range";
+    score--;
   } else if (guessNumber > actualNumber) {
     document.querySelector(".main-content__message").innerHTML = "📈Too High..";
+    score--;
+    document.querySelector(".main-content__score").innerHTML = score;
   } else if (guessNumber < actualNumber) {
     document.querySelector(".main-content__message").innerHTML = "📉Too Low..";
+    score--;
+    document.querySelector(".main-content__score").innerHTML = score;
   }
 }
 
@@ -28,9 +34,12 @@ document
   .querySelector(".header__play-again")
   .addEventListener("click", function () {
     actualNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector(".main-content__actual-number").innerText = "?";
     document.querySelector(".main-content__message").innerHTML =
       "Start Guessing...";
     document.querySelector(".main").style.backgroundColor = "black";
     document.querySelector(".Guess").style.backgroundColor = "black";
     document.querySelector(".Guess").value = "";
+    score = 20;
+    document.querySelector(".main-content__score").innerHTML = score;
   });
