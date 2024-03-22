4469421460f98401c5a45873952cbf1c617825cd
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-03-22 19:21:01 +0545

    Finalize GuessNumber Project
    Code Refactoring along with different functionalities achieved

--- Learnings from Jonas Schmedtmann/JavaScript/Projects/GuessNumber/main.js
+++ Learnings from Jonas Schmedtmann/JavaScript/Projects/GuessNumber/main.js
@@ -1,50 +1,69 @@
 "use strict";
+
 let actualNumber = Math.trunc(Math.random() * 20) + 1;
+let guess = document.querySelector(".Guess");
 let score = 20;
 let highScore = 0;
-let mainBackgroundStyle = document.querySelector(".main");
-let message = document.querySelector(".main-content__message");
-let actualNumberMsg = document.querySelector(".main-content__actual-number");
-let inputBackground = document.querySelector(".Guess");
-let scoreMsg = document.querySelector(".main-content__score");
-let highScoreMsg = document.querySelector(".main-content__highscore");
+
+function displayMsg(className, message) {
+  document.querySelector("." + className).textContent = message;
+}
+
+function styleBackground(className, color) {
+  document.querySelector("." + className).style.backgroundColor = color;
+}
 
 document
-  .querySelector(".main-content__check")
-  .addEventListener("click", function () {
-    let guessNumber = Number(document.querySelector(".Guess").value);
+  .querySelector(".main-content__check").addEventListener("click", function () {
+
+    let guessNumber = Number(guess.value);
+
     if (actualNumber === guessNumber) {
-      if (highScore < score) highScore = score;
-      mainBackgroundStyle.style.backgroundColor = "green";
-      message.textContent = "🎉 Correct Guess!!";
-      actualNumberMsg.textContent = actualNumber;
-      inputBackground.style.backgroundColor = "green";
-      scoreMsg.textContent = score;
-      highScoreMsg.textContent = highScore;
-    } else if (guessNumber > 20 || guessNumber <= 0 || !guessNumber)
-      message.textContent = "⛔Number Out of Range";
+
+      if (highScore < score)
+        highScore = score;
+
+      styleBackground("main", "green");
+      displayMsg("main-content__message", "🎉 Correct Guess!!");
+      displayMsg("main-content__actual-number", actualNumber);
+      styleBackground("Guess", "green");
+      displayMsg("main-content__score", score);
+      displayMsg("main-content__highscore", highScore);
+
+    }
+
+    else if (guessNumber > 20 || guessNumber <= 0 || !guessNumber)
+      displayMsg("main-content__message", "⛔Number Out of Range");
+
     else if (guessNumber != actualNumber) {
+
       if (score > 1) {
-        message.textContent =
-          guessNumber > actualNumber ? "📈Too High.." : "📉Too Low..";
+
+        displayMsg("main-content__message", guessNumber > actualNumber ? "📈Too High.." : "📉Too Low..");
         score--;
-        scoreMsg.textContent = score;
-      } else {
-        scoreMsg.textContent = "0";
-        message.textContent = "💥GAME OVER!!";
+        displayMsg("main-content__score", score);
+
+      }
+
+      else {
+
+        displayMsg("main-content__score", "0");
+        displayMsg("main-content__message", "💥GAME OVER!!");
+
       }
     }
   });
 
-document
-  .querySelector(".header__play-again")
-  .addEventListener("click", function () {
-    actualNumber = Math.trunc(Math.random() * 20) + 1;
-    actualNumberMsg.textContent = "?";
-    message.textContent = "Start Guessing...";
-    mainBackgroundStyle.style.backgroundColor = "black";
-    inputBackground.style.backgroundColor = "black";
-    document.querySelector(".Guess").value = "";
-    score = "20";
-    scoreMsg.textContent = score;
-  });
+document.querySelector(".header__play-again").addEventListener("click", function () {
+
+  actualNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMsg("main-content__actual-number", "?");
+  displayMsg("main-content__message", "Start Guessing...");
+  styleBackground("main", "black");
+  styleBackground("Guess", "black");
+  guess.value = "";
+  score = "20";
+  displayMsg("main-content__score", score);
+
+});
