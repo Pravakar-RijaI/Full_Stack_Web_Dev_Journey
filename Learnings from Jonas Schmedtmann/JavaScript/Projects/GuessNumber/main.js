a5a8e6c8c35eb964db6d578701e14c70b56f6b88
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-03-22 15:35:21 +0545

    Update with grid and js

--- Learnings from Jonas Schmedtmann/JavaScript/Projects/GuessNumber/main.js
+++ Learnings from Jonas Schmedtmann/JavaScript/Projects/GuessNumber/main.js
@@ -1,16 +1,36 @@
 "use strict";
+let actualNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 0;
+let highScore = 0;
+
 function checkNumber() {
-  let actualNumber = Math.trunc(Math.random() * 20) + 1;
-  console.log(actualNumber);
   let guessNumber = Number(document.querySelector(".Guess").value);
-  console.log(guessNumber);
   if (actualNumber === guessNumber) {
     document.querySelector(".main").style.backgroundColor = "green";
     document.querySelector(".main-content__message").innerHTML =
-      "Correct Guess!!";
+      "🎉 Correct Guess!!";
+    document.querySelector(".main-content__actual-number").innerText =
+      actualNumber;
+    document.querySelector(".Guess").style.backgroundColor = "green";
+    score++;
+    document.querySelector(".main-content__score").innerHTML = score;
+  } else if (guessNumber > 20 || guessNumber <= 0) {
+    document.querySelector(".main-content__message").innerHTML =
+      "⛔Number Out of Range";
   } else if (guessNumber > actualNumber) {
     document.querySelector(".main-content__message").innerHTML = "📈Too High..";
   } else if (guessNumber < actualNumber) {
     document.querySelector(".main-content__message").innerHTML = "📉Too Low..";
   }
 }
+
+document
+  .querySelector(".header__play-again")
+  .addEventListener("click", function () {
+    actualNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector(".main-content__message").innerHTML =
+      "Start Guessing...";
+    document.querySelector(".main").style.backgroundColor = "black";
+    document.querySelector(".Guess").style.backgroundColor = "black";
+    document.querySelector(".Guess").value = "";
+  });
