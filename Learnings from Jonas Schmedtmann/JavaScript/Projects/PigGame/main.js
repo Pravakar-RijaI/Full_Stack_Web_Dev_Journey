7f4c5a711cc1f819077bff3f1a3765a2251b1f35
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-03-23 23:13:41 +0545

    Add basic Functionality
    Basic Functionality of adding scores and switching players were added and some CSS was also edited for active area.

--- Learnings from Jonas Schmedtmann/JavaScript/Projects/PigGame/main.js
+++ Learnings from Jonas Schmedtmann/JavaScript/Projects/PigGame/main.js
@@ -1,85 +1,36 @@
 'use strict';
 
-
 let newgameBtn = document.querySelector('.controls__newgame');
-let player1CurrentScore = document.querySelector('.player1area__currentscore');
-let player2CurrentScore = document.querySelector('.player2area__currentscore');
-let player1FinalScore = document.querySelector('.player1area__finalscore');
-let player2FinalScore = document.querySelector('.player2area__finalscore');
-let player1BgClr = document.querySelector('.player1area');
-let player2BgClr = document.querySelector('.player2area');
+
 let figureDice = document.querySelector('.figure__dice');
-let player1CScore = 0;
-let player2CScore = 0;
-let player1FScore = 0;
-let player2FScore = 0;
+let player1Area = document.querySelector('.player1area');
+let player2Area = document.querySelector('.player2area');
+let currentScore = 0;
+let activePlayer = 0;
 
 let rollDiceBtn = document.querySelector('.controls__rolldice');
 let holdBtn = document.querySelector('.controls__hold');
 
-newgameBtn.addEventListener("click", function () {
-    player1CurrentScore.textContent = "0";
-    player1FinalScore.textContent = "0";
-    player2CurrentScore.textContent = "0";
-    player2FinalScore.textContent = "0";
-    figureDice.classList.add('hidden');
-    player1CScore = 0;
-    player2CScore = 0;
-});
 
-let turn = 1;
-const player1Turn = function () {
-    //pic display
-    player1BgClr.style.backgroundColor = "rgba(245, 245, 245, 0.788)";
-    let randomNumber = Math.trunc(Math.random() * 6) + 1;
-    console.log(randomNumber);
-    if (randomNumber != 1) {
-        player1CScore += randomNumber;
-        player1CurrentScore.textContent = player1CScore;
-    }
-    else {
-        turn = 0;
-    }
-}
-
-const player2Turn = function () {
-    player2BgClr.style.backgroundColor = "rgba(245, 245, 245, 0.788)";
-    let randomNumber = Math.trunc(Math.random() * 6) + 1;
-    console.log(randomNumber);
-    //pic display
-    if (randomNumber != 1) {
-        player2CScore += randomNumber;
-        player2CurrentScore.textContent = player2CScore;
-    }
-    else {
-        turn = 1;
-    }
-}
-
+//Rolling Dice Functionality
 rollDiceBtn.addEventListener("click", function () {
-    if (turn)
-        player1Turn();
-    else
-        player2Turn();
-});
+    //Generating random dice roll
+    let dice = Math.trunc(Math.random() * 6) + 1;
 
-holdBtn.addEventListener("click", function () {
-    if (turn) {
-        player1FScore += player1CScore;
-        player1FinalScore.textContent = player1FScore;
-        player1CScore = 0;
-        player1CurrentScore.textContent = "0";
-        player1BgClr.style.backgroundColor = "rgba(245, 245, 245, 0.438)";
-        turn = 0;
-        player2Turn();
+    //Display dice
+    figureDice.classList.remove('hidden');
+    figureDice.src = `./img/dice-${dice}.png`;
+
+    //Check if 1 and change turn
+    if (dice != 1) {
+        currentScore += dice;
+        document.querySelector(`.player${activePlayer + 1}area__currentscore`).textContent = currentScore;
     }
     else {
-        player2FScore += player2CScore;
-        player2FinalScore.textContent = player2FScore;
-        player2CScore = 0;
-        player2CurrentScore.textContent = "0";
-        player2BgClr.style.backgroundColor = "rgba(245, 245, 245, 0.438)";
-        turn = 1;
-        player1Turn();
+        currentScore = 0;
+        document.querySelector(`.player${activePlayer + 1}area__currentscore`).textContent = currentScore;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player1Area.classList.toggle('player-active');
+        player2Area.classList.toggle('player-active');
     }
 })
