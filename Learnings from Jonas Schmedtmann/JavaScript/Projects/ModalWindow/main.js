60842998452a1ee1e2cf7a57af93f70274880182
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-03-23 15:40:28 +0545

    Add Final Code

---
+++ Learnings from Jonas Schmedtmann/JavaScript/Projects/ModalWindow/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let showModalBtn = document.querySelectorAll('.show-modal');
+let modalWindow = document.querySelector('.modal-window');
+let closeModalBtn = document.querySelector('.close-modal');
+let overlay = document.querySelector('.overlay');
+
+const showModal = function () {
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < showModalBtn.length; i++) {
+    showModalBtn[i].addEventListener("click", showModal);
+}
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+    if (!modalWindow.classList.contains("hidden") && event.key === "Escape") {
+        console.log(event.key);
+        closeModal();
+    }
+});
