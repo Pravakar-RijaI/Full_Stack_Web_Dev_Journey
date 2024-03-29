8976359ded0ef05c68ad6b866da9ee674934ee5c
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-03-29 21:46:43 +0545

    Add Partially Full Functionality

--- Learnings from Jonas Schmedtmann/JavaScript/Challenges/Challenge 4/main.js
+++ Learnings from Jonas Schmedtmann/JavaScript/Challenges/Challenge 4/main.js
@@ -1,38 +1,18 @@
 let submitBtn = document.querySelector('.main__submit');
+let displayTextArea = document.querySelector('.textdisplay');
 
 function onSubmit() {
     let input = document.querySelector('.textinput').value;
-    input = input.trim().toLowerCase();
-    // console.log(input);
-    const inputVariablesArray = input.split('\n');
-    // console.log(inputVariablesArray);
-    let variablesArray;
-    let variables;
-    let variable;
-    let camelCase = "";
-    let camelCaseArray = [];
-    for (let i = 0; i < inputVariablesArray.length; i++) {
-        variables = inputVariablesArray[i].trim();
-        variablesArray = variables.split('_');
-        if (variablesArray.length >= 2) {
-            for (let i = 0; i <= 1; i++) {
-                variable = variablesArray[i];
-                variable = i == 0 ? variable.toLowerCase() : variable.replace(variable.slice(0, 1), variable.slice(0, 1).toUpperCase());
-                camelCase += variable;
-                variable = "";
-            }
-        }
-        camelCaseArray.push(camelCase);
-        camelCase = "";
-    }
-    let i = 0;
-    for (let i = 0; i < 5; i++) {
+    const inputVariablesArray = input.toLowerCase().split('\n');
+    let displayText = "";
 
-        for (let j = 0; j <= i; j++) {
-            console.log(`\n✅ ${i} ${j}`);
-
-        }
+    for (const variables of inputVariablesArray) {
+        const [first, second] = variables.split('_');
+        const variable = `${first.trim().toLowerCase()}${second.trim().replace(second[0], second[0].toUpperCase())}`
+        console.log(variable);
+        displayText += variable + "\n";
     }
+    displayTextArea.value = displayText;
 }
 
 submitBtn.addEventListener("click", onSubmit);
