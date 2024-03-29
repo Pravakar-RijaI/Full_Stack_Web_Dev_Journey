192cfabdce4009d334897e5abdc51f46d826b6ba
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-03-29 21:59:53 +0545

    Add padding display to console

--- Learnings from Jonas Schmedtmann/JavaScript/Challenges/Challenge 4/main.js
+++ Learnings from Jonas Schmedtmann/JavaScript/Challenges/Challenge 4/main.js
@@ -6,10 +6,10 @@ function onSubmit() {
     const inputVariablesArray = input.toLowerCase().split('\n');
     let displayText = "";
 
-    for (const variables of inputVariablesArray) {
+    for (const [i, variables] of inputVariablesArray.entries()) {
         const [first, second] = variables.split('_');
         const variable = `${first.trim().toLowerCase()}${second.trim().replace(second[0], second[0].toUpperCase())}`
-        console.log(variable);
+        console.log(variable.padEnd(20), `${"✅".repeat(i + 1)}`);
         displayText += variable + "\n";
     }
     displayTextArea.value = displayText;
