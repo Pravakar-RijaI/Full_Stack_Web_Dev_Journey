fa1a34bb1d501e9e5b932c1a16dbdb42fdeaacd7
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-04-03 23:55:25 +0545

    Add Basic Files

---
+++ Learnings from Jonas Schmedtmann/JavaScript/Challenges/Challenge 6/main.js
@@ -0,0 +1,17 @@
+//Challenge 1
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+
+dogsJulia.forEach(function (value, index) {
+    if (value >= 3)
+        console.log(`${index + 1} dog is an adult.`)
+    else
+        console.log(`${index + 1} dog is a puppy.`)
+});
+
+dogsJulia.forEach(function (value, index) {
+    if (value >= 3)
+        console.log(`${index + 1} dog is an adult.`)
+    else
+        console.log(`${index + 1} dog is a puppy.`)
+});
